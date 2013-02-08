c798524e1ac707208f2d1cf24ac4e36aa6ba2ccc
Michal Kowalkowski <kowalkowski.michal@gmail.com>
2013-02-08 20:37:15 +0800

    Uploading files

--- README.md
+++ README.md
@@ -1,4 +1,154 @@
 rateThis
 ========
-
 rateThis is a jquery plugin that allows you to easy create rating functionality for your form.
+
+Browser Support
+--------
+
+rateThis has been tested and should work in
+- Internet Explorer 8+
+- Safari
+- Firefox
+- Chrome
+- Opera
+
+Examples
+--------
+
+You can find some examples [here](http://www.michalkowalkowski.com/rateThis).
+
+Usage
+--------
+
+Include the code before `</body>` tag
+
+```html
+<script src="/path/to/rateThis.min.js"></script>
+```
+  			
+Then just add it to one of DOMs elements
+
+```html
+<script>
+$('#elementsID').rateThis();
+</script>
+```			
+
+
+API
+--------
+Settable Options
+<table>
+<thead>
+<tr>
+<th>Option</th>
+<th>Description</th>
+<th>Data Type</th>
+<th>Default</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>fullImg</td>
+<td>Name of image file to use for displaying current rating</td>
+<td>string</td>
+<td>'full.png'</td>
+</tr>						
+<tr>
+<td>emptyImg</td>
+<td>Name of image file to use for displaying possible rating</td>
+<td>string</td>
+<td>'empty.png'</td>
+</tr>						
+<tr>
+<td>zero</td>
+<td>If true, will display image to set value of rating to zero</td>
+<td>boolean</td>
+<td>false</td>
+</tr>						
+<tr>
+<td>zeroImg</td>
+<td>Name of image file to use for displaying zero rating</td>
+<td>string</td>
+<td>'zero.png'</td>
+</tr>						
+<tr>
+<td>value</td>
+<td>Default value for rating</td>
+<td>integer</td>
+<td>1</td>
+<tr>
+</tr>	
+<td>max</td>
+<td>Max value for rating</td>
+<td>integer</td>
+<td>5</td>
+</tr>	
+<tr>	
+<td>disabled</td>
+<td>If true, rating will be displayed as disabled</td>
+<td>boolean</td>
+<td>false</td>	
+</tr>	
+<tr>
+<td>disabledFullImg</td>
+<td>Name of image file to use for displaying current rating when disabled</td>
+<td>string</td>
+<td>'full.png'</td>
+</tr>						
+<tr>
+<td>disabledEmptyImg</td>
+<td>Name of image file to use for displaying possible rating when disabled</td>
+<td>string</td>
+<td>'empty.png'</td>
+</tr>						
+<tr>
+<td>disabledZeroImg</td>
+<td>Name of image file to use for displaying zero rating when disabled</td>
+<td>string</td>
+<td>'zero.png'</td>
+</tr>						
+</tbody>
+</table>
+
+Methods
+
+<table>
+<thead>
+<tr>
+<th>Method</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>disable</td>
+<td>Disables the rating</td>
+</tr>						
+<tr>
+<td>enable</td>
+<td>Enables the rating</td>
+</tr>						
+<tr>
+<td>update</td>
+<td>Updates plugin view, for new input value</td>
+</tr>						
+<tr>
+<td>uncheck</td>
+<td>Unchecks the checkbox</td>
+</tr>						
+<tr class="last">
+<td>destroy</td>
+<td>Removes plugin functionality</td>
+</tr>						
+</tbody>
+</table>
+
+				
+License
+-------
+rateThis plugin is released under [MIT license](http://opensource.org/licenses/mit-license.php).
+
+Credits
+-------
+rateThis plugin was created by [Michał Kowalkowski](https://github.com/michalkow). You can contact me at [kowalkowski.michal@gmail.com](mailto:kowalkowski.michal@gmail.com)
---
+++ rateThis.js
@@ -0,0 +1,194 @@
+(function( $ ){
+
+  var methods = {
+    init : function( options ) {
+	 
+	// Default settings 
+	 var settings = $.extend( {
+      fullImg : 'full.png',
+	  emptyImg : 'empty.png',
+	  zero : false,
+	  zeroImg : 'zero.png',
+	  value : 1,
+	  max: 5,
+	  disabled : false,
+	  disabledZeroImg : 'zero.png',
+	  disabledFullImg: 'full.png',
+	  disabledEmptyImg: 'empty.png',
+    }, options);
+
+       return this.each(function(){
+			var root = this;
+			var $this=$(this);
+
+			root.max=settings.max;
+			root.disabled=settings.disabled;
+			root.cvalue=settings.value;
+			root.zero=settings.zero;
+			root.fullImg=settings.fullImg;
+			root.emptyImg=settings.emptyImg;
+			root.zeroImg=settings.zeroImg;
+			root.disabledZeroImg=settings.disabledZeroImg;
+			root.disabledFullImg=settings.disabledFullImg;
+			root.disabledEmptyImg=settings.disabledEmptyImg;
+			
+			$this.attr('type', 'hidden');
+			$this.after('<div class="rateThis"></div>');
+			
+			if(root.disabled) {
+				var zeroImg = root.disabledZeroImg;
+				var fullImg = root.disabledFullImg;
+				var emptyImg = root.disabledEmptyImg;
+				$this.prop('disabled', true);
+			} else {
+				var zeroImg = root.zeroImg;
+				var fullImg = root.fullImg;
+				var emptyImg = root.emptyImg;						
+			}
+
+			if(root.zero) {
+				$this.next().append('<a href="#" style="border:none" class="rateThis-obj rateThis-0"><img src="'+zeroImg+'" /></a>');
+			}
+			
+			for(i=1;i<=root.max; i++) {
+				if(i>root.cvalue) {
+					$this.next().append('<a href="#" style="border:none" class="rateThis-obj rateThis-'+i+'"><img src="'+emptyImg+'" /></a>');
+				} else {
+					$this.next().append('<a href="#" style="border:none" class="rateThis-obj rateThis-'+i+'"><img src="'+fullImg+'" /></a>');
+				}
+			}
+			
+
+			$this.next().find('.rateThis-obj').each(function( index ){
+				if(!root.zero) index++;
+				$(this).on('click', function() { rate( index, 0 ); return false; });
+				$(this).on('mouseenter', function() { rate( index, 2 ); });
+				$(this).on('mouseout', function() { rate( parseInt(root.cvalue), 1) });
+			});
+			
+			function rate(rating, hover) {
+				if(!root.disabled) {
+					var from = rating;
+					if(root.zero) from=rating+1;
+					if(rating>root.cvalue) {
+						for(i=0;i<from;i++) {
+							if(!$this.next().find('.rateThis-obj').eq(i).hasClass('rateThis-0')) {
+								$this.next().find('.rateThis-obj').eq(i).find('img').attr('src', root.fullImg);
+							}	
+						}
+					} else if(root.cvalue>=rating && hover<2) {
+						for(i=from;i<=root.max;i++) {
+								$this.next().find('.rateThis-obj').eq(i).find('img').attr('src', root.emptyImg);
+						}
+					}
+					if(!hover) {
+						root.cvalue=rating;
+						$this.val(rating);
+					}
+				}
+			}
+
+			return this;				
+       });
+    },
+	
+    destroy : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				$this.attr('type', 'text');
+				$this.next().remove();
+				return this;
+       });
+    },
+	
+	disable : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				if(root.zero) {
+					$this.next().find('.rateThis-obj').eq(0).find('img').attr('src', root.disabledZeroImg);
+					var val = root.cvalue;
+				} else {
+					var val = root.cvalue-1;
+				}
+				$this.next().find('.rateThis-obj').each(function( index ){
+					if(!$this.next().find('.rateThis-obj').eq(index).hasClass('rateThis-0')) {
+						if(index>val) {
+							$this.next().find('.rateThis-obj').eq(index).find('img').attr('src', root.disabledEmptyImg);
+						} else {
+							$this.next().find('.rateThis-obj').eq(index).find('img').attr('src', root.disabledFullImg);
+						}	
+					}					
+				});
+				root.disabled=true;
+				$this.prop('disabled', true);
+				return this;
+       });
+    },
+
+	enable : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				if(root.zero) {
+					$this.next().find('.rateThis-obj').eq(0).find('img').attr('src', root.zeroImg);
+					var val = root.cvalue;
+				} else {
+					var val = root.cvalue-1;
+				}
+				$this.next().find('.rateThis-obj').each(function( index ){
+					if(!$this.next().find('.rateThis-obj').eq(index).hasClass('rateThis-0')) {
+						if(index>val) {
+							$this.next().find('.rateThis-obj').eq(index).find('img').attr('src', root.emptyImg);
+						} else {
+							$this.next().find('.rateThis-obj').eq(index).find('img').attr('src', root.fullImg);
+						}	
+					}					
+				});
+				root.disabled=false;
+				$this.prop('disabled', false);
+				return this;
+       });
+    },
+	
+	update : function( ) {
+       return this.each(function(){
+				var root = this;
+				var $this=$(this);
+				var rating = parseInt($this.val());
+				var from = rating;
+				if(root.zero) from=rating+1;
+				if(rating>root.cvalue) {
+					for(i=0;i<from;i++) {
+						if(!$this.next().find('.rateThis-obj').eq(i).hasClass('rateThis-0')) {
+							$this.next().find('.rateThis-obj').eq(i).find('img').attr('src', root.fullImg);
+						}	
+					}
+				} else if(root.cvalue>=rating) {
+					for(i=from;i<=root.max;i++) {;
+						$this.next().find('.rateThis-obj').eq(i).find('img').attr('src', root.emptyImg);
+					}
+				}
+				root.cvalue=rating;
+
+				return this;
+       });
+    },
+	
+	
+  };
+
+  $.fn.rateThis = function( method ) {
+
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.rateThis' );
+    }    
+  
+  };
+
+})( jQuery );
